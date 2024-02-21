da29526b05eb12e609a10f18c961f6a9cd43899d
MUKE-coder <gmukejohnbaptist@gmail.com>
2024-02-21 10:11:23 +0300

    added cards

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -22,40 +22,40 @@ export default function Hero() {
         <div className="container px-5 py-6 pb-14 text-left transition-opacity duration-300 sm:px-0 sm:py-20 sm:text-center">
           <p className="-mt-4 sm:-mt-10 mb-7">
             <Link
-              href="#"
+              href="https://www.youtube.com/@JBWEBDEVELOPER"
               className="rounded-md border border-dashed border-purple-700 px-3 py-1.5 text-purple-400 transition-colors hover:border-gray-700 hover:text-white"
             >
               <span className="relative -top-[0.5px] mr-1 text-xs font-semibold uppercase text-white">
-                New
+                Welcome
               </span>
               <span className="hidden sm:inline">
-                Announcing new road maps for teams
-                <span className="font-semibold">Learn more</span>
+                to Our new Desishub Docs
+                <span className="font-semibold ms-2">By JB Web Developer</span>
               </span>
               <span className="inline sm:hidden">
-                Announcing roadmap for teams
+                Welcome to Our new Desishub Docs by JB
               </span>
             </Link>
           </p>
           <h className="mb-2 bg-gradient-to-b from-amber-50 to-purple-500 bg-clip-text text-2xl font-bold text-transparent sm:mb-4 sm:text-5xl">
-            Developer Roadmaps
+            All in One Developer's Handbook
           </h>
           <p className="hidden px-4 text-lg text-gray-400 sm:block">
-            <span className="font-medium text-gray-400">roadmap.sh</span>
-            is a community effort to create roadmaps, guides and other
-            educational content to help guide developers in picking up a path
-            and guide their learnings.
+            Master any dev challenge with Desishub's all-in-one handbook. From
+            roadmaps to video tutorials, conquer frontend, backend, and design
+            with ease. This guide empowers you with everything you need to
+            become a top developer.
           </p>
           <p className="text-md block px-0 text-gray-400 sm:hidden">
-            Community created roadmaps, guides and articles to help developers
-            grow in their career.
+            Desishub's all-in-one handbook. From roadmaps to video tutorials,
+            conquer frontend, backend, and design with ease.
           </p>
         </div>
       </div>
       <div className="relative border-b border-b-[#1e293c] py-10 sm:py-14">
         <div className="max-w-7x mx-auto">
           <h1 className="text-md font-regular absolute -top-[17px] flex rounded-lg border border-[#1e293c] bg-slate-900 px-3 py-1 text-slate-400 sm:left-1/2 sm:-translate-x-1/2">
-            Role based Roadmap
+            Popular Bookmarks
           </h1>
           <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 mx-auto px-8">
             {links.map((item, i) => {
